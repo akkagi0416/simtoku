45fff7d394bf68b73cf09a373102ed0a3405c8e0
akkagi0416 <akkagi0416@gmail.com>
2015-06-07 16:58:45 +0900

    toppage design

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
         <!--[if lt IE 9]>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
         <![endif]-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
         <?php wp_head(); ?>
@@ -31,21 +32,29 @@
     wp_nav_menu( $args );
 ?>
             </div>
-            <div class="container">
-                <div class="breadcrumb">
-<?php
-    if( function_exists( 'bcn_display' ) ){
-        bcn_display();
-    }
-?>
-                </div>
-            </div>
-            <div class="img_main">
-                <div class="container">
-                    <!--
-                    <img src="<?php bloginfo( 'template_url' ); ?>/img/main.jpg" alt="" class="img-responsive">
-                    -->
-                    <img src="http://lorempixel.com/600/200/technics/" alt="" class="img-responsive">
-                </div>
-            </div>
         </header>
+        <div class="img_main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>格安SIMで毎月5,000円のお得なスマホ生活</h2>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4">
+                        <img src="http://lorempixel.com/600/200/technics/" alt="" class="img-responsive">
+                    </div>
+                </div>
+                <!--
+                <img src="<?php bloginfo( 'template_url' ); ?>/img/main.jpg" alt="" class="img-responsive">
+                -->
+            </div>
+        </div>
+        <div class="container">
+            <div class="breadcrumb">
+<?php
+if( function_exists( 'bcn_display' ) ){
+    bcn_display();
+}
+?>
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -2,58 +2,85 @@
 <div class="container">
     <div class="row">
         <div class="col-md-9 main">
-            <h2>まずは自分に合ったSIMを見つけましょう</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
-                        <div class="caption">
-                            <h3>Thumbnail label</h3>
-                            <p>Dolor tempore tempora ullam quos dolore, ullam labore, possimus deserunt. Aspernatur voluptate iste saepe alias repellendus repellat ducimus assumenda atque!</p>
-                            <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
-                        </div>
-                    </div><!-- //.thumbnail -->
+<?php
+    if( have_posts() ) :
+        while( have_posts() ) : the_post()
+?>
+            <?php the_content(); ?>
+<?php
+        endwhile;
+    endif;
+?>
+<!--
+            <section>
+                <h2>格安SIMのここがすごい</h2>
+                <ul>
+                    <li>スマホ代が毎月5,000円もお得</li>
+                    <li>最新スマホでも使える</li>
+                    <li>音声付SIMならMNPもできる</li>
+                </ul>
+                <img src="http://placehold.jp/600x200.png" alt="" class="img-responsive">
+                <p>MVNO各社からさまざまな格安なSIMプランが提案されています。自分の生活スタイルに合わせたSIMプランを選んで、月々のスマホ料金を格安に押さえましょう。</p>
+            </section>
+
+            <section>
+                <h2>自分に合った格安SIMを探しましょう</h2>
+                <p>Sit corporis rem nulla elit illum. Nemo elit voluptatibus ipsam.</p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>SIMのイロハ</h3>
+                                <p>格安SIMについて説明しています。「SIMとは?」「なぜお得なの?」そんな疑問にお答えします。これから格安SIMを始めてみたい人、スマホ料金を見直してみたい人、そんな人はまずこちらからどうぞ。</p>
+                                <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
+                            </div>
+                        </div><!-- //.thumbnail -->
+                    </div>
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3><i class="fa fa-folder-open-o"></i>プラン選び</h3>
+                                <p>格安SIMを使うにあたってMVNO各社からいろんなプランが提案されています。こちらのページでは、各社のプラン比較や紹介をしています。こちらから自分に最適な格安SIMプランを見つけてみましょう。</p>
+                                <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
+                            </div>
+                        </div><!-- //.thumbnail -->
+                    </div>
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
+                            <div class="caption">
+                                <h3>スマホ選び</h3>
+                                <p>格安SIMが使えるスマホをご紹介。格安スマホやコスパ重視、さらにはiPhoneまでいろんなSIMフリーのスマホを紹介しています。新しいスマホが欲しくなったらまずはこちらをチェック。</p>
+                                <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
+                            </div>
+                        </div><!-- //.thumbnail -->
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
-                        <div class="caption">
-                            <h3>Thumbnail label</h3>
-                            <p>Dolor tempore tempora ullam quos dolore, ullam labore, possimus deserunt. Aspernatur voluptate iste saepe alias repellendus repellat ducimus assumenda atque!</p>
-                            <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
-                        </div>
-                    </div><!-- //.thumbnail -->
+            </section>
+            <section>
+                <h2>SIMが見つかったら始めてみましょう</h2>
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#"><img src="http://placehold.jp/200x200.png" alt=""></a>
+                    </div>
+                    <div class="media-body">
+                        <h4>スタートガイド</h4>
+                        <p>Sit corporis laudantium quaerat et reprehenderit debitis quibusdam ea tempore sapiente vel. Soluta exercitationem quasi veniam eum veritatis non iure.</p>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.jp/200x200.png" alt="" class="img-responsive">
-                        <div class="caption">
-                            <h3>Thumbnail label</h3>
-                            <p>Dolor tempore tempora ullam quos dolore, ullam labore, possimus deserunt. Aspernatur voluptate iste saepe alias repellendus repellat ducimus assumenda atque!</p>
-                            <p><a href="#" class="btn btn-primary">こちらへ&raquo;</a></p>
-                        </div>
-                    </div><!-- //.thumbnail -->
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#"><img src="http://placehold.jp/200x200.png" alt=""></a>
+                    </div>
+                    <div class="media-body">
+                        <h4>よくわる質問</h4>
+                        <p>Sit corporis laudantium quaerat et reprehenderit debitis quibusdam ea tempore sapiente vel. Soluta exercitationem quasi veniam eum veritatis non iure.</p>
+                    </div>
                 </div>
-            </div>
-            <h2>SIMが見つかったら始めてみましょう</h2>
-            <div class="media">
-                <div class="media-left">
-                    <a href="#"><img src="http://placehold.jp/200x200.png" alt=""></a>
-                </div>
-                <div class="media-body">
-                    <h4>スタートガイド</h4>
-                    <p>Sit corporis laudantium quaerat et reprehenderit debitis quibusdam ea tempore sapiente vel. Soluta exercitationem quasi veniam eum veritatis non iure.</p>
-                </div>
-            </div>
-            <div class="media">
-                <div class="media-left">
-                    <a href="#"><img src="http://placehold.jp/200x200.png" alt=""></a>
-                </div>
-                <div class="media-body">
-                    <h4>よくわる質問</h4>
-                    <p>Sit corporis laudantium quaerat et reprehenderit debitis quibusdam ea tempore sapiente vel. Soluta exercitationem quasi veniam eum veritatis non iure.</p>
-                </div>
-            </div>
+            </section>
+-->
         </div><!-- //.main -->
         <?php get_sidebar(); ?>
     </div><!-- //.row -->

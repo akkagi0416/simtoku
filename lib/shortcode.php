85cb971d4adc4b143da0dfc8b80760ab08e2f98b
akkagi0416 <akkagi0416@gmail.com>
2015-07-23 18:35:22 +0900

    modify shortcode @ option

--- lib/shortcode.php
+++ lib/shortcode.php
@@ -1,8 +1,8 @@
 <?php
 
-// require_once get_template_directory() . '/lib/mvnodb.php';
-// echo get_template_directory() . '/lib/mvnodb.php';
-
+/*
+ * _sは簡略化バージョン
+ */
 function mvno_func( $atts )
 {
     extract( shortcode_atts( array(
@@ -13,11 +13,12 @@ function mvno_func( $atts )
     $mvnoInfo = $m->getInfo( $shortname );
     $mvnoPlan = $m->getPlan( $shortname );
 
-    $html = '<section class="mvno_s_info">';
+    $html = '<section class="mvno_info">';
     $html .= mvno_head( $mvnoInfo );
     $html .= mvno_mybtn( $mvnoInfo );
     $html .= mvno_feature( $mvnoInfo );
-    $html .= mvno_table_s( $mvnoInfo, $mvnoPlan );
+    $html .= mvno_table( $mvnoInfo, $mvnoPlan );
+    $html .= mvno_option( $mvnoInfo, $mvnoPlan );
     $html .= '</section>';
 
     return $html;
@@ -32,7 +33,7 @@ function mvno_s_func( $atts ){
     $mvnoInfo = $m->getInfo( $shortname );
     $mvnoPlan = $m->getPlan( $shortname );
 
-    $html = '<section class="mvno_s_info">';
+    $html = '<section class="mvno_info">';
     $html .= mvno_head( $mvnoInfo );
     $html .= mvno_mybtn_s( $mvnoInfo );
     $html .= mvno_feature( $mvnoInfo );
@@ -105,6 +106,46 @@ function mvno_feature( $mvnoInfo )
 EOM;
     return $html;
 }
+function mvno_table( $mvnoInfo, $mvnoPlan )
+{
+    $html = <<< EOM2
+        <h3>{$mvnoInfo['mvno']}の基本プラン</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-condensed">
+                <tr class="active">
+                    <th>プラン名</th>
+                    <th>音声通話</th>
+                    <th>月額料金(円)</th>
+                    <th>最大速度</th>
+                    <th>データ量</th>
+                    <th>最低契約期間</th>
+                </tr>
+EOM2;
+
+    foreach( $mvnoPlan as $row ){
+        // 基本プランのみ表示
+        // if( $row['plan_basic'] == 1 ){
+            // データSIM or 音声通話付SIM表示 (SMS付SIMなし)
+            if( $row['sim_data'] == 1 || $row['sim_voice'] == 1 ){
+                $table .= '<tr>';
+                $html .= '<td>' . $row['plan_name'] . '</td>';
+                if( $row['sim_voice'] == 1 ){
+                    $html .= '<td>〇</td>';
+                } else {
+                    $html .= '<td>&nbsp</td>';
+                }
+                $html .= '<td>' . $row['cost']      . '</td>';
+                $html .= '<td>' . $row['speed']     . '</td>';
+                $html .= '<td>' . $row['data']      . '</td>';
+                $html .= '<td>' . $row['contract_period']      . '</td>';
+                $html .= '</tr>';
+            }
+        // }
+    }
+    $html .= '</table></div>';
+
+    return $html;
+}
 function mvno_table_s( $mvnoInfo, $mvnoPlan )
 {
     $html = <<< EOM2
@@ -144,6 +185,51 @@ EOM2;
     return $html;
 }
 
+function mvno_option( $mvnoInfo, $mvnoPlan )
+{
+    $html = <<< EOM
+    <div class="option">
+        <h3>{$mvnoInfo['mvno']}の機能一覧</h3>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <th>プラン変更</th>
+                    <td>{$mvnoInfo['option1']}</td>
+                </tr>
+                <tr>
+                    <th>データ繰り越し</th>
+                    <td>{$mvnoInfo['option2']}</td>
+                </tr>
+                <tr>
+                    <th>バースト機能</th>
+                    <td>{$mvnoInfo['option3']}</td>
+                </tr>
+                <tr>
+                    <th>高速通信ON/OFF</th>
+                    <td>{$mvnoInfo['option4']}</td>
+                </tr>
+                <tr>
+                    <th>3日間の速度制限</th>
+                    <td>{$mvnoInfo['option5']}</td>
+                </tr>
+                <tr>
+                    <th>音声通話の割引</th>
+                    <td>{$mvnoInfo['option6']}</td>
+                </tr>
+                <tr>
+                    <th>シェアプラン</th>
+                    <td>{$mvnoInfo['option7']}</td>
+                </tr>
+                <tr>
+                    <th>Wi-Fiスポット</th>
+                    <td>{$mvnoInfo['option8']}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+EOM;
+    return $html;
+}
 
 add_shortcode( 'mvno_s', 'mvno_s_func' );
 add_shortcode( 'mvno',   'mvno_func' );

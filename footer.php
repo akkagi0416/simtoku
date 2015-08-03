04f438aabeef7f4aeb5325d1b7a814cdec700652
akkagi0416 <akkagi0416@gmail.com>
2015-08-03 11:15:24 +0900

    Script move header to footer

--- footer.php
+++ footer.php
@@ -19,5 +19,11 @@ jQuery(function(){
 });
 </script>
 <?php wp_footer(); ?>
+<!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,12 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
-        <!--[if lt IE 9]>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <![endif]-->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
         <?php wp_head(); ?>
     </head>
 <?php
